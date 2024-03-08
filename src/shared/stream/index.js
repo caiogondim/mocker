e7c2f18f4ae4297cd7f9da3f35a0c10fba46b2cb
Caio Gondim <me@caiogondim.com>
2024-03-07 22:40:38 -0500

    fix: TS

--- src/shared/stream/index.js
+++ src/shared/stream/index.js
@@ -1,7 +1,6 @@
 const delay = require('./delay')
 const passAndSave = require('./pass-and-save')
 const pipeline = require('./pipeline')
-const tee = require('./tee')
 const throttle = require('./throttle')
 const rewindable = require('./rewindable')
 
@@ -9,7 +8,6 @@ module.exports = {
   delay,
   passAndSave,
   pipeline,
-  tee,
   throttle,
   rewindable,
 }
