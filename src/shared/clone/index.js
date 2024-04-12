4f655b1473634e8e47871f7562da8d2d35fc01b1
Caio Gondim <me@caiogondim.com>
2024-04-11 23:18:01 -0400

    Default value for responses dir arg (#13)
    * chore: adds ERR_TLS_CERT_ALTNAME_INVALID to non-fatal errors
    
    * chore: change responsesDir and overwriteRequestHeaders default values

--- src/shared/clone/index.js
+++ src/shared/clone/index.js
@@ -1,5 +1,7 @@
 /** @typedef {import('./types').Clone} Clone */
 
+// @TODO: replace this with `structuredClone`
+
 /** @type {Clone} */
 function clone(source) {
   return JSON.parse(JSON.stringify(source))
